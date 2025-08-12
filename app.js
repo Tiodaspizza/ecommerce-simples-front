6c901bfd54af90ff85879ec5ebde1d1453a8a8d0
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2025-08-12 11:17:40 -0300

    projeto quase finalizado

--- app.js
+++ app.js
@@ -1,47 +1,87 @@
-let carrosel = document.querySelector(".carousel-container");
-let cards = document.querySelectorAll(".favorite-card");
-let buttonprevious = document.querySelector("#button-previous");
-let buttonNext = document.querySelector("#button-next");
+function initFavoriteCarousel() {
+  let carrosel = document.querySelector(".carousel-container");
+  let cards = document.querySelectorAll(".favorite-card");
+  let buttonprevious = document.querySelector("#button-previous");
+  let buttonNext = document.querySelector("#button-next");
+  let currentIndex = 0; // index atual
+  let cardsWidth = cards[0].offsetWidth; // tamanho do card individua
+  let gapCarrosel = parseInt(getComputedStyle(carrosel).gap); // pega o tamanho do gap do elemento pai
+  let totalCardStep = cardsWidth + gapCarrosel; // soma tamanho toal card  com o gap do elemento pai
+  let carroselWidth = carrosel.offsetWidth; // pega o tamanho do elemento pai
+  let cardsVisiveis = Math.floor(carroselWidth / totalCardStep); // divide tamanho do elemento pai pela soma do tamanho do card e o gap do elemento pai
+  let maxIndex = cards.length - cardsVisiveis; // numero cards 7 - cards visiveis 4 = 3 0123, 1234 ,2345, 3456
+  let maxDeslocamento = -(cards.length * totalCardStep - carroselWidth);
 
-let currentIndex = 0; // index atual
+  function updateCarouselPosition() {
+    // funcao para dar update carrosel fazer o deslocamento
+    let deslocamento = -currentIndex * totalCardStep;
+    if (deslocamento < maxDeslocamento) {
+      deslocamento = maxDeslocamento;
+      currentIndex = cards.length - cardsVisiveis;
+    }
 
-let cardsWidth = cards[0].offsetWidth; // tamanho do card individua
-let gapCarrosel = parseInt(getComputedStyle(carrosel).gap); // pega o tamanho do gap do elemento pai
-let totalCardStep = cardsWidth + gapCarrosel; // soma tamanho toal card  com o gap do elemento pai
-let carroselWidth = carrosel.offsetWidth; // pega o tamanho do elemento pai
-let cardsVisiveis = Math.floor(carroselWidth / totalCardStep); // divide tamanho do elemento pai pela soma do tamanho do card e o gap do elemento pai
-let maxIndex = cards.length - cardsVisiveis; // numero cards 7 - cards visiveis 4 = 3 0123, 1234 ,2345, 3456
-let maxDeslocamento = -(cards.length * totalCardStep - carroselWidth);
+    if (deslocamento > 0) {
+      deslocamento = 0;
+      currentIndex = 0;
+    }
 
-function updateCarouselPosition() {
-  // funcao para dar update carrosel fazer o deslocamento
-  let deslocamento = -currentIndex * totalCardStep;
-  if (deslocamento < maxDeslocamento) {
-    deslocamento = maxDeslocamento;
-    currentIndex = cards.length - cardsVisiveis;
+    carrosel.style.transform = `translateX(${deslocamento}px)`;
   }
 
-  if (deslocamento > 0) {
-    deslocamento = 0;
-    currentIndex = 0;
-  }
+  buttonNext.addEventListener("click", () => {
+    // adicionando o botao de evento ao clicar para fazer a transicao
+    currentIndex++;
+    if (currentIndex >= maxIndex) {
+      currentIndex = 0;
+    }
+    updateCarouselPosition();
+  });
 
-  carrosel.style.transform = `translateX(${deslocamento}px)`;
+  buttonprevious.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = maxIndex;
+    }
+    updateCarouselPosition();
+  });
+}
+initFavoriteCarousel();
+
+let carouselDepoiment = document.querySelector(".carousel-talking-wrapper");
+let carouselCardsDepoiment = document.querySelectorAll(
+  ".carousel-taking-cards"
+);
+let btnNextDepoiment = document.querySelector("#btnNext-depoiment-carousel");
+let btnPreviousDepoiment = document.querySelector(
+  "#btnPrevious-depoiment-carousel"
+);
+console.log(carouselCardsDepoiment);
+let currentIndex = 0;
+
+function updateCarousel() {
+  carouselCardsDepoiment.forEach((card) => {
+    card.classList.add("hidden");
+  });
+
+  carouselCardsDepoiment[currentIndex].classList.remove("hidden");
 }
 
-buttonNext.addEventListener("click", () => {
-  // adicionando o botao de evento ao clicar para fazer a transicao
+btnNextDepoiment.addEventListener("click", () => {
   currentIndex++;
-  if (currentIndex >= maxIndex) {
+  if (currentIndex > carouselCardsDepoiment.length - 1) {
     currentIndex = 0;
   }
-  updateCarouselPosition();
+  updateCarousel();
 });
 
-buttonprevious.addEventListener("click", () => {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = maxIndex;
+
+btnPreviousDepoiment.addEventListener('click',()=>{
+  currentIndex-- 
+  if(currentIndex < 0){
+    currentIndex = carouselCardsDepoiment.length-1
   }
-  updateCarouselPosition();
-});
+
+  updateCarousel()
+})
+
+
